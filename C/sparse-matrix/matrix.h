477e1e7a38fac1db56d759ce6a102513be4ae15b
Maicon <mpml@aluno.ifnmg.edu.br>
2022-12-23 15:47:00 -0300

    matriz.h functions description updated

--- C/sparse-matrix/matrix.h
+++ C/sparse-matrix/matrix.h
@@ -23,30 +23,29 @@ typedef struct {
 /* Initialize a matrix of zeros with nrow rows and ncol columns. */
 void initialize_matrix(Matrix* matrixPtr, int nrow, int ncol);
 
-/* Deallocates the memory used by matrixPtr. After that, initialize
-   matriPtr as a matrix of zeros with nrow rows and ncol columns*/
+/* Deallocates memory used by matrix Ptr. After that initialize
+ * matrix Ptr as a matrix of zeros with nrow rows and ncol columns*/
 void clear_matrix(Matrix* matrixPtr, int nrow, int ncol);
 
-/* Define that the element on row "row" and column "col" receives "val".
-   The parametre row < nRows and col < nCols */
+/* Defines that the element of line "row" and column "col" receives "val".
+ * The parameter row < nRows and col < nCols */
 void define_element(Matrix matrix, int row, int col, int val);
 
-/* Callculate the sum of matrix a and b putting the result in resPtr.
-   Free the memory previous used by resPtr and sobrewrite resPtr.*/
+/* Calculates the sum of matrices a and b putting the result in resPtr.
+ * Frees memory previously used by resPtr and overwrites resPtr.*/
 bool sum(Matrix *resPtr, Matrix a, Matrix b);
 
-/* Callculate the subtraction of matrix a and b putting the result in resPtr.
-   Free the memory previously used by resPtr and sobrewrites resPtr.*/
+/* Calculates the subtraction of the matrices a and b by placing the
+ * result in resPtr. Frees memory previously used by resPtr and overwrites resPtr.*/
 bool sub(Matrix *resPtr, Matrix a, Matrix b);
 
-/* Callculates the multiplication of matrix a and b putting the result in resPtr.
-   Free the memory previously used by resPtr and sobrewrite resPtr. */
+/* Calculates the multiplication of matrices a and b by placing the result in resPtr.
+   Frees memory previously used by resPtr and overwrites resPtr. */
 bool mult(Matrix *resPtr, Matrix a, Matrix b);
 
 /* Print the matrix mat on the format below:
  * (nrow=100,ncol=200,[0,3]=5,[0,8]=10,[2,9]=50,[5,3]=10,[5,15]=20,[99,0]=33)
- * where this matrux has 100 rows and 200 columns and the only
- * non zero elements are:
+ * wich has 100 rows and 200 columns and the only non-zero elements are:
  * - in row 0, column 3 with value 5
  * - in row 0, column 8 with value 10
  * - in row 2, column 9 with value 50
